e08d9b9d8cdc859d96078cf5c2adc460b57fb1d0
sigmotoa <11698886+sigmotoa@users.noreply.github.com>
2023-09-04 09:22:08 -0500

    Added green to btn2

--- app/src/main/java/com/sigmotoa/navalg9/MainActivity.java
+++ app/src/main/java/com/sigmotoa/navalg9/MainActivity.java
@@ -3,6 +3,7 @@ package com.sigmotoa.navalg9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -17,8 +18,15 @@ public class MainActivity extends AppCompatActivity {
         Button btnR0C1 = findViewById(R.id.btn_2);
 
         btnR0C0.setOnClickListener(v -> {
-                btnR0C0.setBackgroundColor(getResources().getColor(R.color.rojo));
+                btnR0C0.setBackgroundColor(getColor(R.color.rojo));
                 btnR0C0.setImageDrawable(getDrawable(R.drawable.baseline_tag_faces_24));
+        });
+
+        btnR0C1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnR0C1.setBackgroundColor(getColor(R.color.verde));
+            }
         });
 
     }
